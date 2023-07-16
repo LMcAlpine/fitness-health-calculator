6b35d6215ff511173c5bbbc57f27c9de7e37f6da
Luke <lukedmcalpine@gmail.com>
2023-07-16 15:40:59 -0700

    caloriesburned route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const port = 3000;
 const bmi = require("./routes/bmi.js");
 const bodyfat = require("./routes/bodyfat.js");
 const idealweight = require("./routes/idealweight.js");
+const caloriesburned = require("./routes/caloriesburned.js");
 
 // route definition
 app.get("/", (req, res) => {
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
 app.use("/bmi", bmi);
 app.use("/bodyfat", bodyfat);
 app.use("/idealweight", idealweight);
+app.use("/caloriesburned", caloriesburned);
 
 // start server
 app.listen(port, () => {
--- server/routes/caloriesburned.js
+++ server/routes/caloriesburned.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// idealweight page route
+// caloriesburned page route
 router.get("/", function (req, res) {
-  res.send("idealweight page");
+  res.send("caloriesburned page");
 });
 
 module.exports = router;
