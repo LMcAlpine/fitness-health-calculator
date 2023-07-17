b3638b749001568f726d882866fd9d7236ad6ba9
Luke <lukedmcalpine@gmail.com>
2023-07-16 19:49:24 -0700

    ideal weight calculator

---
+++ server/utils/idealweightcalculator.js
@@ -0,0 +1,14 @@
+const calculateIdealWeight = function (gender, height) {
+  const baseWeightMaleKG = 50;
+  const baseWeightFemaleKG = 45.5;
+  const kgPerInch = 2.3;
+
+  let idealWeight = gender === "male" ? baseWeightMaleKG : baseWeightFemaleKG;
+
+  for (let inch = 0; inch < height.inches; inch++) {
+    idealWeight += kgPerInch;
+  }
+  return idealWeight;
+};
+
+// console.log(calculateIdealWeight("male", { feet: 5, inches: 10 }));
